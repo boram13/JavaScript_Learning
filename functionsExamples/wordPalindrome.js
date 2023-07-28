fa7f36dde26221b65fe0d73095dfa3e5d725008b
boram13 <bmenerja@helius-systems.com>
2023-07-28 15:05:05 +0200

    function call un wordPalindrome exercise

--- functionsExamples/wordPalindrome.js
+++ functionsExamples/wordPalindrome.js
@@ -1,15 +1,21 @@
-function wordPalindrome(str){
-
-    let  x = str.length -1;
-
+function palindrome(str)
+{
     for(let i = 0; i < str.length; i++){
-       
+
+        let  x = str.length -1;
         if (str[i] !== str[x - i])
         {
             return false;
         }
-    }
-    return true;
+    }return true;
+
+}
+
+
+function wordPalindrome(str){
+
+     return palindrome(str)
+    
 }
 let inputFunction = wordPalindrome("abdbba")
 console.log(inputFunction)
